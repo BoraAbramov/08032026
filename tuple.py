b0b9094aa7791a1443b6fa4707b7e3b8ab16c5aa
BoraAbramov <borya812@gmail.com>
2026-03-08 19:36:35 +0200

    update

---
+++ tuple.py
@@ -0,0 +1,80 @@
+
+# 1 create tuple with 3 of your favorite movies
+movie = ("westworld", "moon knight", "reacher")
+
+# 2 create tuple with all countries in Europe (ask ChatGPT)
+europe_countries = (
+    "Albania",
+    "Andorra",
+    "Austria",
+    "Belarus",
+    "Belgium",
+    "Bosnia and Herzegovina",
+    "Bulgaria",
+    "Croatia",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Iceland",
+    "Ireland",
+    "Italy",
+    "Kosovo",
+    "Latvia",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Malta",
+    "Moldova",
+    "Monaco",
+    "Montenegro",
+    "Netherlands",
+    "North Macedonia",
+    "Norway",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "San Marino",
+    "Serbia",
+    "Slovakia",
+    "Slovenia",
+    "Spain",
+    "Sweden",
+    "Switzerland",
+    "Ukraine",
+    "United Kingdom",
+    "Vatican City"
+)
+
+# 3 create tuple with all the days of feb (1, 2 ... 28)
+feb_days = (
+    1, 2, 3, 4, 5, 6, 7,
+    8, 9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28
+)
+
+# 4 create tuple with all the days of dec (1, 2 .. 31)
+dec_days = (
+    1, 2, 3, 4, 5, 6, 7,
+    8, 9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28,
+    29, 30, 31
+)
+
+# 5 create tuple of all the month in the year
+months = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+# 6 create tuple of all USA presidents till today (ask ChatGPT), use len to find out how many are they
+
+
+# 7 use mean to find the avg of this tuple = (8, 11, -3, 12)
+
+movie = ("westworld", "moon knight", "reacher")
+
